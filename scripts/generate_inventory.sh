6df6d60c892e083bceb9ac1c53d8785f7b2999c9
venkatram9652 <automation9490@gmail.com>
2026-02-17 13:16:59 +0000

    update

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+set -e
+
+echo "Generating Ansible inventory..."
 
 cd terraform
+
 IPS=$(terraform output -json web_ips | jq -r '.[]')
 
-echo "[web]" > ../ansible/inventory/hosts.ini
+cd ..
+
+mkdir -p ansible/inventory
+
+echo "[web]" > ansible/inventory/hosts.ini
 
 for ip in $IPS
 do
-  echo "$ip ansible_user=root" >> ../ansible/inventory/hosts.ini
+  echo "$ip ansible_user=root" >> ansible/inventory/hosts.ini
 done
+
+echo "Inventory created:"
+cat ansible/inventory/hosts.ini
